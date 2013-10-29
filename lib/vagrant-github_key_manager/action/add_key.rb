d4f36ab10dcbf8a8aa08ebc160faf36dcedbc96e
haruyama-makoto <makoto.haruyama@dena.com>
2013-10-29 20:51:23 +0900

    Fix curl options

--- lib/vagrant-github_key_manager/action/add_key.rb
+++ lib/vagrant-github_key_manager/action/add_key.rb
@@ -18,7 +18,7 @@ module VagrantPlugins
           script = ["GITHUB_USERNAME=$1", "GITHUB_PASSWORD=$2"]
           script.concat(["ssh-keygen -N '' -f ~/.ssh/github_rsa", "ssh-add ~/.ssh/github_rsa"])
           script.concat(["set -- $(<~/.ssh/github_rsa.pub)", 'JSON=\'{"title":"vagrant provision key", "key": "ssh-rsa \'$2\'"}\''])
-          script.concat(['curl -u "$GITHUB_USERNAME:$GITHUB_PASSWORD" -d "$JSON "', endpoint, '> ~/.ssh/git.response'])
+          script.concat(['curl -u "$GITHUB_USERNAME:$GITHUB_PASSWORD" -d "$JSON" ' + "#{endpoint}" + ' > ~/.ssh/git.response'])
 
           # if we have a successful response, skip generating key
           unless @machine.communicate.test('grep '"id"' ~/.ssh/git.response')
