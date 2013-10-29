5a14293f1423fd3f7c07284936e7184b8ca2721f
haruyama-makoto <makoto.haruyama@dena.com>
2013-10-29 17:25:36 +0900

    Add remove key

--- lib/vagrant-github_key_manager/action/remove_key.rb
+++ lib/vagrant-github_key_manager/action/remove_key.rb
@@ -9,7 +9,15 @@ module VagrantPlugins
         end
 
         def call(env)
-          @ui.info("not yet implemented")
+          config = env[:global_config].github_key_manager
+          endpoint = config.endpoint
+
+          @ui.info("We will now generate an ssh key and add it to your github account.")
+          gitUsername = @ui.ask("What is your github account name? ")
+          gitPassword = @ui.ask_noecho("What is your github account password? (not stored) ")
+
+          @machine.communicate.execute("curl -X DELETE -u #{gitUsername}:#{gitPassword} #{endpoint}")
+          @ui.info("delete ssh key")
           @app.call(env)
         end
 
